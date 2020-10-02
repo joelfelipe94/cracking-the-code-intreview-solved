54b0025ce320a26ab943a919817ddb54849eed02
joel gaya <joelfelipe94@gmail.com>
2020-10-01 23:25:09 -0300

    include the exercise 2.2

--- chapter 2/chapter2Test.cpp
+++ chapter 2/chapter2Test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "exercise1.hpp"
+#include "exercise2.hpp"
 
 // Exercise 1
 
@@ -34,6 +35,25 @@ TEST(exercise1, removeDuplicatesInplaceEmpty)
     removeDuplicatesInPlace(list);
     GTEST_ASSERT_EQ(list, expected);
 }
+
+TEST(exercise2, getNthElementFromEnd)
+{
+    auto list = forward_list<int>{1,2,3,4,5,6,7,8,9,0};
+    auto N = 4;
+    auto expected = 7;  
+    auto result = getNthElementFromEnd(list,4);
+    GTEST_ASSERT_EQ(result, expected);
+}
+
+TEST(exercise2, getNthElementFromEndLast)
+{
+    auto list = forward_list<int>{1,2,3,4,5,6,7,8,9,0};
+    auto N = 10;
+    auto expected = 1;  
+    auto result = getNthElementFromEnd(list,N);
+    GTEST_ASSERT_EQ(result, expected);
+}
+
 
 
 int main(int argc, char* argv[])
---
+++ chapter 2/exercise2.hpp
@@ -0,0 +1,26 @@
+// Implement an algorithm to find the kth to last element of a singly linked list.
+#include <forward_list>
+#include <cassert>
+using namespace std;
+
+template <typename T>
+size_t getLength(const forward_list<T>& list){
+    auto length = size_t(0);
+    for(auto value : list){
+        length++;
+    }
+    return length;
+}
+
+template <typename T>
+T getNthElementFromEnd(forward_list<T>& list, size_t N){
+    auto length = getLength(list);
+    assert(N > 0 && N <= length); // we count from zero to the maximum size
+    auto K = length - N;
+    auto count = size_t(0);
+    for(auto it=list.begin();it!=list.end();it++, count++){
+        if(count==K){
+           return *it; 
+        }
+    }
+}
